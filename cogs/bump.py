eb08719390d08ff6d6eb78b35cb63efb365d509c
Joyte <longdylanp99@gmail.com>
2021-03-10 22:47:57 +1300

    Remove excess code, fix wrong indent

--- cogs/bump.py
+++ cogs/bump.py
@@ -197,45 +197,24 @@ class Bump(commands.Cog):
             )
         top.add_field(name="Leaderboard", value=printstring, inline=False)
         top.set_thumbnail(url=ctx.guild.icon_url)
+        
         try:
             await message.edit(embed=top, content=None)
+
         except discord.errors.HTTPException:
-            top = discord.Embed(
-                title=f"Top {tnum} bump totals for `{ctx.guild.name}`",
-                description="These are the top bump totals for this guild. `!d bump` to try and get on the leaderboard!"
-            )
-        top.set_thumbnail(url=ctx.guild.icon_url)
-
-        printstring = [string+"\n" for string in printstring.split("\n")]
-
-        printstring_1, printstring_2 = printstring[:len(printstring)//2], printstring[len(printstring)//2:]
-        printstring_1, printstring_2 = "".join(printstring_1), "".join(printstring_2)
-
-        top.add_field(name="Leaderboard", value=printstring_1, inline=False)
-        top.add_field(name="\u200e", value=printstring_2, inline=False)
-
-        try:
-            await message.edit(embed=top, content=None)
-        except discord.errors.HTTPException:
-            printstring_1 = [string + "\n" for string in printstring_1.split("\n")]
-            printstring_2 = [string + "\n" for string in printstring_2.split("\n")]
-
-            printstring_3, printstring_4 = printstring_2[:len(printstring_2) // 2], printstring_2[len(printstring_2) // 2:]
-            printstring_1, printstring_2 = printstring_1[:len(printstring_1) // 2], printstring_1[len(printstring_1) // 2:]
-
-            printstring_1, printstring_2 = "".join(printstring_1), "".join(printstring_2)
-            printstring_3, printstring_4 = "".join(printstring_3), "".join(printstring_4)
-
             top = discord.Embed(
                 title=f"Top {tnum} bump totals for `{ctx.guild.name}`",
                 description="These are the top bump totals for this guild. `!d bump` to try and get on the leaderboard!"
             )
             top.set_thumbnail(url=ctx.guild.icon_url)
 
+            printstring = [string+"\n" for string in printstring.split("\n")]
+
+            printstring_1, printstring_2 = printstring[:len(printstring)//2], printstring[len(printstring)//2:]
+            printstring_1, printstring_2 = "".join(printstring_1), "".join(printstring_2)
+
             top.add_field(name="Leaderboard", value=printstring_1, inline=False)
             top.add_field(name="\u200e", value=printstring_2, inline=False)
-            top.add_field(name="\u200e", value=printstring_3, inline=False)
-            top.add_field(name="\u200e", value=printstring_4, inline=False)
 
             try:
                 await message.edit(embed=top, content=None)
