3bb3f39af6875d12cd89757de53a1ab51ef070da
Joyte <longdylanp99@gmail.com>
2021-03-11 16:13:52 +1300

    Typo

--- cogs/bump.py
+++ cogs/bump.py
@@ -130,7 +130,7 @@ class Bump(commands.Cog):
 
     @commands.command()
     async def topbumptotal(self, ctx: commands.Context, tnum: int = 10):
-        if tnum == 0:
+        if tnum == 1:
             bump = Bmp(ctx.guild.id)
             await self.bumptotal(ctx, bump.get_top(1))
             return
